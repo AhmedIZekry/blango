2b53f871e904bd62bdc0140d13304b2561f6af48
Ahmed IIbrahim <ahmediiizekry@gmail.com>
2024-09-15 02:25:55 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -2,29 +2,36 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+import logging 
+logger = logging.getLogger(__name__) 
+
+
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 
 def post_detail(request,slug):
 
-  post = get_object_or_404(Post,slug=slug)
+    post = get_object_or_404(Post,slug=slug)
+    logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+)
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
 
-  if request.user.is_active:
-    if request.method == "POST":
-        comment_form = CommentForm(request.POST)
-
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.content_object = post
-            comment.creator = request.user
-            comment.save()
-            return redirect(request.path_info)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
     else:
-        comment_form = CommentForm()
-  else:
-      comment_form = None
+        comment_form = None
 
-  return render(request,"blog/post-detail.html",{'post':post,"comment_form": comment_form})
+    return render(request,"blog/post-detail.html",{'post':post,"comment_form": comment_form})
